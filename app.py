d282b227ceccd265beb15319678651a2399746e2
Rathore925 <rathoreharsh564@gmail.com>
2023-10-17 18:05:00 +0530

    firts commit

---
+++ app.py
@@ -0,0 +1,9 @@
+import numpy as np 
+import cv2
+import mediapipe as mp
+from collections import deque
+
+bpoints = [deque(maxlen=1024)]
+gpoints = [deque(maxlen=1024)]
+bpoints = [deque(maxlen=1024)]
+bpoints = [deque(maxlen=1024)]
